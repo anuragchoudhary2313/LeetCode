1c4b5a6de0055a2051ce1296a1eb6018c778b07e
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2024-07-19 09:42:00 +0530

    Time: 64 ms (60.96%), Space: 27.4 MB (59.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
 
-        for (int i = 0; i < n - 1; ++i) {
+        for (int i = 0; i < n ; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue; 
             }
