51dded9936b0b2103313a8e50aa89c4a9c1d28a2
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2024-07-19 09:41:37 +0530

    Time: 65 ms (57.94%), Space: 27.4 MB (59.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-   vector<vector<int>> threeSum(vector<int>& nums) {
-        if (nums.size() < 3) return {};
-        vector<vector<int>> ans;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i + 2 < nums.size(); ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int l = i + 1;
-            int r = nums.size() - 1;
-            while (l < r) {
-                const int sum = nums[i] + nums[l] + nums[r];
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n - 1; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; 
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    ans.push_back({nums[i], nums[l++], nums[r--]});
-                    while (l < r && nums[l] == nums[l - 1]) ++l;
-                    while (l < r && nums[r] == nums[r + 1]) --r;
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        ++j; 
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        --k; 
+                    }
+                    ++j;
+                    --k;
                 } else if (sum < 0) {
-                    ++l;
+                    ++j;
                 } else {
-                    --r;
+                    --k;
                 }
             }
         }
+
         return ans;
     }
 };
