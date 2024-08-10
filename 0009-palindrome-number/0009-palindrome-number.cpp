3b4d15d3c25765367f9d4442641e524937ab73af
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2024-08-10 12:09:14 +0530

    Time: 13 ms (24.04%), Space: 10.5 MB (10.95%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
+        std::string str = std::to_string(x);
         int n = str.length();
         int i = 0;
         int j = n - 1;
-
         while (i <= j) {
             if (str[i] == str[j]) {
                 i++;
