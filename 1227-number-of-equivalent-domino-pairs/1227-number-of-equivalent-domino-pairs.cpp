e2d8a7c6acfd7efea142cceeafe0c41c065e76f9
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2024-08-11 10:32:59 +0530

    Time: 29 ms (88.58%), Space: 25.7 MB (56.9%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        for (int i = 0; i < dominoes.size(); ++i) {
-            int a = std::min(dominoes[i][0], dominoes[i][1]);
-            int b = std::max(dominoes[i][0], dominoes[i][1]);
-            dominoes[i][0] = a;
-            dominoes[i][1] = b;
-        }
-        sort(dominoes.begin(), dominoes.end());
+        vector<int> count(100, 0);
         int sum = 0;
-        for (int i = 1, count = 1; i < dominoes.size(); ++i) {
-            if (dominoes[i] == dominoes[i - 1]) {
-                sum += count;
-                count++;
-            } else {
-                count = 1;
-            }
+        for (int i = 0; i < dominoes.size(); ++i) {
+            int a = max(dominoes[i][0], dominoes[i][1]);
+            int b = min(dominoes[i][0], dominoes[i][1]);
+            int index = a * 10 + b; 
+            sum += count[index]++;
         }
+
         return sum;
     }
 };
