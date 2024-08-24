83a8f6c8eb482deb3cb77d5d144363791b0e0a78
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2024-08-24 20:56:04 +0530

    Time: 4 ms (66.45%), Space: 11.9 MB (57.27%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int thirdMax(std::vector<int>& nums) {
-        std::sort(nums.begin(), nums.end(), std::greater<int>());
-        nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(), nums.end(),greater<int>());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
         if (nums.size() >= 3) {
             return nums[2];
         } else {
