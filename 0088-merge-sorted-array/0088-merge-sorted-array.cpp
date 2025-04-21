f40d4125d2ebd569524dc5bd35874dbaceddee62
Anurag Choudhary <147531998+anuragchoudhary2313@users.noreply.github.com>
2025-04-21 17:08:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (39.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> totalnums(m+n) ;
-        
-         ::merge(nums1.begin(), nums1.begin() + m, nums2.begin(), nums2.begin() + n, totalnums.begin());
-           for (int i = 0; i < m + n; ++i) {
-            nums1[i] = totalnums[i];
+        int i = m - 1; 
+        int j = n - 1; 
+        int k = m + n - 1; 
+
+        while (j >= 0) { 
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
     }
 };
